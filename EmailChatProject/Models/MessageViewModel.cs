6a75c28972b4f546ce053cfe96ccbff3cd7eceae
Erkut Çakar <erkutcakar@gmail.com>
2025-05-16 17:22:33 +0300

    ......

---
+++ EmailChatProject/Models/MessageViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EmailChatProject.Models
+{
+    public class MessageViewModel
+    {
+
+            public int SendMessage;
+            public int ReceiveMessage;
+            public int DeleteMessage;
+
+        
+    }
+}
